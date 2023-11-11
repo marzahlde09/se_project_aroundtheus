6fd4868ec7b36813a08dcce31e2b74cbda60b51b
Daniel Marzahl <marzahlde09@gmail.com>
2023-11-11 10:52:38 -0600

    Add open and close functionality to modal block

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".modal__close-button");
+const modalBlock = document.querySelector(".modal");
+
+editButton.addEventListener("click", function () {
+  modalBlock.classList.add("modal_opened");
+});
+
+closeButton.addEventListener("click", function () {
+  modalBlock.classList.remove("modal_opened");
+});
