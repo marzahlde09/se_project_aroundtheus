0c5bed15b6d6092eaf15a6ecc589aabc89bc0ae5
Daniel Marzahl <marzahlde09@gmail.com>
2023-11-14 20:34:27 -0600

    feat: add cards to page through add form

--- scripts/index.js
+++ scripts/index.js
@@ -29,6 +29,7 @@ const modalBlock = document.querySelector(".modal");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button");
 const editProfileModal = document.querySelector(".form_type_profile");
 const profileModalCloseButton = editProfileModal.querySelector(
   ".form__close-button"
@@ -41,6 +42,11 @@ const profileModalDescription = editProfileModal.querySelector(
 );
 const profileModalSubmitButton =
   editProfileModal.querySelector(".form__submit");
+const addModal = document.querySelector(".form_type_add");
+const addModalCloseButton = addModal.querySelector(".form__close-button");
+const addModalTitle = addModal.querySelector(".form__field:first-of-type");
+const addModalLink = addModal.querySelector(".form__field:last-of-type");
+const addModalCreateButton = addModal.querySelector(".form__submit");
 const cardGallery = document.querySelector(".gallery__cards");
 const cardTemplate = document.querySelector("#card").content;
 
@@ -56,6 +62,16 @@ profileModalCloseButton.addEventListener("click", function () {
 });
 
 editProfileModal.addEventListener("submit", submitProfileForm);
+
+addButton.addEventListener("click", function () {
+  openModal(addModal);
+});
+
+addModalCloseButton.addEventListener("click", function () {
+  closeModal(addModal);
+});
+
+addModal.addEventListener("submit", submitAddForm);
 
 function openModal(modal) {
   modalBlock.classList.add("modal_opened");
@@ -74,6 +90,21 @@ function submitProfileForm(event) {
   profileDescription.textContent = profileModalDescription.value;
 
   closeModal(editProfileModal);
+}
+
+function submitAddForm(event) {
+  event.preventDefault();
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImageLink = addModalLink.value;
+  const cardName = addModalTitle.value;
+
+  cardElement.querySelector(".card__image").src = cardImageLink;
+  cardElement.querySelector(".card__image").alt = cardName;
+  cardElement.querySelector(".card__title").textContent = cardName;
+
+  cardGallery.prepend(cardElement);
 }
 
 function getCardElement(data) {
