2cb2e20ea72971b255c118388b8f576d22330d13
Daniel Marzahl <marzahlde09@gmail.com>
2023-11-18 10:39:02 -0600

    feat: form validation and closing forms by escape or click outside

--- scripts/index.js
+++ scripts/index.js
@@ -58,12 +58,27 @@ editButton.addEventListener("click", function () {
   profileFormName.value = profileName.textContent;
   profileFormDescription.value = profileDescription.textContent;
 
+  const inputList = Array.from(
+    profileForm.querySelectorAll(".modal__form-field")
+  );
+  const buttonElement = profileForm.querySelector(".modal__form-submit");
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    checkInputValidity(profileForm, inputElement);
+  });
+
   openModal(profileForm);
 });
 
 profileForm.addEventListener("submit", submitProfileForm);
 
 addButton.addEventListener("click", function () {
+  const inputList = Array.from(cardForm.querySelectorAll(".modal__form-field"));
+  const buttonElement = cardForm.querySelector(".modal__form-submit");
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    checkInputValidity(cardForm, inputElement);
+  });
   openModal(cardForm);
 });
 
@@ -140,3 +155,88 @@ closeButtons.forEach((button) => {
 });
 
 initialCards.forEach((card) => cardGallery.append(getCardElement(card)));
+
+//form validation
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(
+    `.${inputElement.name}-input-error`
+  );
+  inputElement.classList.add("modal__form-field_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("modal__form-input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(
+    `.${inputElement.name}-input-error`
+  );
+  inputElement.classList.remove("modal__form-field_type_error");
+  errorElement.textContent = "";
+  errorElement.classList.remove("modal__form-input-error_active");
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    console.log(inputElement.validity.valid);
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("button_inactive");
+  } else {
+    buttonElement.classList.remove("button_inactive");
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(".modal__form-field")
+  );
+  const buttonElement = formElement.querySelector(".modal__form-submit");
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    const modals = Array.from(document.querySelectorAll(".modal"));
+    modals.forEach((modal) => {
+      closeModal(modal);
+    });
+  }
+});
+
+document.addEventListener("click", function (evt) {
+  const currentPopUp = document.querySelector(".modal_opened");
+  if (evt.target === currentPopUp) {
+    closeModal(currentPopUp);
+  }
+});
+
+enableValidation();
